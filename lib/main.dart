e958b07e0bc513810847c3d17555dc123b91b281
SandroMaglione <lass.maglio@gmail.com>
2023-12-08 05:48:20 +0100

    `FlameGame` setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,29 @@
+import 'dart:async';
+
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    GameWidget(
+      game: MyGame(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyGame extends FlameGame with SingleGameInstance {
+  @override
+  FutureOr<void> onLoad() {
+    // TODO
+  }
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const Placeholder(),
-    );
+  void render(Canvas canvas) {
+    super.render(canvas);
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
   }
 }
