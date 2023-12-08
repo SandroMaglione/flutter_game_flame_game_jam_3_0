73261ed5eb5cb218ce1aaedfd2000a5b611ef376
SandroMaglione <lass.maglio@gmail.com>
2023-12-08 06:50:07 +0100

    4 directions movement

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,35 @@
 import 'dart:async';
 
 import 'package:flame/game.dart';
+import 'package:flame/input.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_game_flame_game_jam_3_0/player.dart';
 
 void main() {
-  runApp(
-    GameWidget(
-      game: MyGame(),
-    ),
-  );
+  runApp(GameWidget(game: MyGame()));
 }
 
 final Player _player = Player();
 
-class MyGame extends FlameGame with SingleGameInstance {
-  @override
-  Color backgroundColor() => Colors.purple;
-
+class MyGame extends FlameGame with SingleGameInstance, KeyboardEvents {
   @override
   FutureOr<void> onLoad() {
     add(_player);
   }
 
   @override
-  void render(Canvas canvas) {
-    super.render(canvas);
-  }
+  KeyEventResult onKeyEvent(
+    RawKeyEvent event,
+    Set<LogicalKeyboardKey> keysPressed,
+  ) {
+    final isKeyDown = event is RawKeyDownEvent;
+    final handled = _player.onKeyEvent(keysPressed);
 
-  @override
-  void update(double dt) {
-    super.update(dt);
+    if (isKeyDown && handled) {
+      return KeyEventResult.handled;
+    }
+
+    return KeyEventResult.ignored;
   }
 }
--- lib/player.dart
+++ lib/player.dart
@@ -1,12 +1,48 @@
 import 'package:flame/components.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_game_flame_game_jam_3_0/direction.dart';
 
 class Player extends SpriteComponent with HasGameRef {
-  Player() : super(size: Vector2.all(50.0));
+  Direction direction = const Idle();
+
+  final double speed;
+  Player({this.speed = 10.0}) : super(size: Vector2.all(50.0));
 
   @override
   Future<void> onLoad() async {
     super.onLoad();
     sprite = await gameRef.loadSprite('logo.png');
     position = gameRef.size / 2;
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    _move(dt);
+  }
+
+  void _move(double delta) {
+    if (direction is! Idle) {
+      position.add(direction.vector2 * speed);
+    }
+  }
+
+  bool onKeyEvent(Set<LogicalKeyboardKey> keysPressed) {
+    if (keysPressed.contains(LogicalKeyboardKey.keyW)) {
+      direction = const Up();
+      return true;
+    } else if (keysPressed.contains(LogicalKeyboardKey.keyS)) {
+      direction = const Down();
+      return true;
+    } else if (keysPressed.contains(LogicalKeyboardKey.keyA)) {
+      direction = const Left();
+      return true;
+    } else if (keysPressed.contains(LogicalKeyboardKey.keyD)) {
+      direction = const Right();
+      return true;
+    }
+
+    direction = const Idle();
+    return false;
   }
 }
